063afe3f3cc3013ed9cfbf79058c6da45dfa883d
Jan Fanslau <jan.fanslau@sevenval.com>
2018-01-18 22:42:54 +0100

    - gnarz

--- hardware/lightBarrier/index.js
+++ hardware/lightBarrier/index.js
@@ -19,14 +19,13 @@ board.on("ready", function() {
 	// "motionstart" events are fired when the "calibrated"
 	// proximal area is disrupted, generally by some form of movement
 	motion.on("motionstart", function() {
-		// console.log("motionstart");
+		console.log("motionstart");
 	});
 
 	// "motionend" events are fired following a "motionstart" event
 	// when no movement has occurred in X ms
 	motion.on("motionend", function() {
-		// console.log("motionend");
-		console.log(counter++);
+		console.log("motionend");
 	});
 
 });
